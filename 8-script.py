65170eefca47450e382d81ac90d16e7549a5e156
ngocdung03 <dungdidom@gmail.com>
2021-04-10 14:56:15 +0900

    Perceptron

--- 8-script.py
+++ 8-script.py
@@ -8,3 +8,18 @@ class Perceptron:
 cool_perceptron = Perceptron()
 print(cool_perceptron)
     
+## Finding the weighted sum of the inputs.
+class Perceptron:
+  def __init__(self, num_inputs=2, weights=[2,1]):
+    self.num_inputs = num_inputs
+    self.weights = weights
+    
+  def weighted_sum(self, inputs):
+    # create variable to store weighted sum
+    weighted_sum = 0
+    for i in range(self.num_inputs):
+      weighted_sum += inputs[i]*self.weights[i]   # why error with self.num_inputs: ‘int’ object is not subscriptable
+      # complete this loop
+    return weighted_sum  
+cool_perceptron = Perceptron()
+print(cool_perceptron.weighted_sum([24, 55]))
