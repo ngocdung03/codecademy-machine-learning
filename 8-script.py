f5ac32e3bfa59f9c458fcfc420478641da34473b
ngocdung03 <dungdidom@gmail.com>
2021-04-11 23:28:09 +0900

    Perceptron

--- 8-script.py
+++ 8-script.py
@@ -29,3 +29,32 @@ class Perceptron:
 cool_perceptron = Perceptron()
 print(cool_perceptron.weighted_sum([24, 55]))
 print(cool_perceptron.activation(52))
+
+## Training error
+class Perceptron:
+  def __init__(self, num_inputs=2, weights=[1,1]):
+    self.num_inputs = num_inputs
+    self.weights = weights
+    
+  def weighted_sum(self, inputs):
+    weighted_sum = 0
+    for i in range(self.num_inputs):
+      weighted_sum += self.weights[i]*inputs[i]
+    return weighted_sum
+  
+  def activation(self, weighted_sum):
+    if weighted_sum >= 0:
+      return 1
+    if weighted_sum < 0:
+      return -1
+    
+  def training(self, training_set):
+    for inputs in training_set:
+      prediction = self.activation(self.weighted_sum(inputs))  #must include 'self.' otherwise error
+      actual = training_set[inputs]
+      error = actual - prediction
+
+      
+cool_perceptron = Perceptron()
+print(cool_perceptron.weighted_sum([24, 55]))
+print(cool_perceptron.activation(52))
